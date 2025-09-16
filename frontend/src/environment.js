ac1fec345b94ed763a6dd629ab42676e48eabe8a
Ritik Tomar <ritikthakur7987@gmail.com>
2025-09-16 20:02:01 +0530

    Deploying Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,5 @@
 let is_Pod = true;
-const server = is_Pod?`"/api/v1/users/"`:"http://localhost:8080/api/v1/users/";
+const server_link = "https://video-conferencing-backend-tzqz.onrender.com";
+const server = is_Pod?`${server_link}/api/v1/users/`:"http://localhost:8080/api/v1/users/";
 
 export default server;
